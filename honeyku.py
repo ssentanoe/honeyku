d93440f490c83a3ceba24bcb1b2b15509d2d7056
Stewart Sentanoe <stewart.sentanoe@gmail.com>
2022-03-23 13:34:17 +0100

    gitignore + hotflix import

--- honeyku.py
+++ honeyku.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from flask import Flask, request, render_template, send_file
+from flask import Flask, request, render_template, send_file, jsonify
 import logging
 import sys
 import os
